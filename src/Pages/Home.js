10a17e5b837b85890821fef79ec09555e6d8c5e2
Sebastian Trasmonte <sebas.trasmonte@gmail.com>
2024-01-16 20:34:13 -0300

    pagina home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,81 @@
+import "bulma/css/bulma.css"
+import "../Styles.css"
 
+function Home() {
 
+    return (
 
-function Home(){
-
-return(
-
-    <div>
-        <h1>Pagina Home</h1>
-    </div>
-)
+        <div class="columns is-multiline is-mobile">
+            <div class="column is-one-quarter">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="../img/8000-AZ-STONE.jpg" alt="ejemplo zaptilla urban street" />
+                        </figure>
+                    </div>
+                    <article class="message is-large">
+                        <div class="message-header">
+                            <p>Urban Street</p>
+                        </div>
+                        <div class="message-body">
+                            Esta categoria es para el uso diario, diseñadas para la calle y las exigencias del dia a dia, con mucha comodidad y soltura, ideales para vos
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <article class="message is-large">
+                        <div class="message-header">
+                            <p>Urban Street</p>
+                        </div>
+                        <div class="message-body">
+                            Esta categoria es para el uso diario, diseñadas para la calle y las exigencias del dia a dia, con mucha comodidad y soltura, ideales para vos
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <article class="message is-large">
+                        <div class="message-header">
+                            <p>Urban Street</p>
+                        </div>
+                        <div class="message-body">
+                            Esta categoria es para el uso diario, diseñadas para la calle y las exigencias del dia a dia, con mucha comodidad y soltura, ideales para vos
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <article class="message is-large">
+                        <div class="message-header">
+                            <p>Urban Street</p>
+                        </div>
+                        <div class="message-body">
+                            Esta categoria es para el uso diario, diseñadas para la calle y las exigencias del dia a dia, con mucha comodidad y soltura, ideales para vos
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
