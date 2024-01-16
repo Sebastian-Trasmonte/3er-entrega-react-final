2891f1bdc02dca024634728c9b5511a746872537
Sebastian Trasmonte <sebas.trasmonte@gmail.com>
2024-01-15 22:11:37 -0300

    firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCvRwY_nudeUbVAp89MPorbix0dWyc-tsQ",
-    authDomain: "productos-d5a22.firebaseapp.com",
-    projectId: "productos-d5a22",
-    storageBucket: "productos-d5a22.appspot.com",
-    messagingSenderId: "569309047816",
-    appId: "1:569309047816:web:8337aa6dec04827640b5bd"
-  };
+  apiKey: "AIzaSyCvRwY_nudeUbVAp89MPorbix0dWyc-tsQ",
+  authDomain: "productos-d5a22.firebaseapp.com",
+  projectId: "productos-d5a22",
+  storageBucket: "productos-d5a22.appspot.com",
+  messagingSenderId: "569309047816",
+  appId: "1:569309047816:web:8337aa6dec04827640b5bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
