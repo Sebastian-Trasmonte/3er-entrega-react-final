f8c0e5937c1e436e3c69d6ce4d8dede7d4b76396
Sebastian Trasmonte <sebas.trasmonte@gmail.com>
2024-01-15 22:10:52 -0300

    firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvRwY_nudeUbVAp89MPorbix0dWyc-tsQ",
+    authDomain: "productos-d5a22.firebaseapp.com",
+    projectId: "productos-d5a22",
+    storageBucket: "productos-d5a22.appspot.com",
+    messagingSenderId: "569309047816",
+    appId: "1:569309047816:web:8337aa6dec04827640b5bd"
+  };
